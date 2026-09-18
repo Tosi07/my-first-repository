ee416b44f6032b5e002df52b891e60f8dbc96281
Tosi07 <metoshielas@gmail.com>
2026-09-17 17:44:54 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Main{
+   public static void main(String[] args){
+   
+   Movie m1= new Movie();
+   
+   m1.title = "Residents Evil";
+   m1.genre = "Sci-Fi";
+   m1.duration = 90;
+   
+   m1.displayInfo();     
+   
+   Movie m2= new Movie();
+   
+   m2.title = "Titanic";
+   m2.genre = "Romance";
+   m2.duration = 195;
+   
+   m2.displayInfo();     
+   
+   
+  Movie m3= new Movie();
+   
+   m3.title = "Minions";
+   m3.genre = "Animation";
+   m3.duration = 91;
+   
+   m3.displayInfo();     
+   
+   
+   
+   
+   }
+
+}
+
+
+
+
+  
